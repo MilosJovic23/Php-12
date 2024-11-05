f6235096f5b18b803fc98088119505544d214c93
MilosJovic23 <milos.mj992@gmail.com>
2024-11-05 11:55:32 +0100

    * small changes

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+
+
+
+
+    $baza = mysqli_connect("localhost","root","","prvi_cas");
+
+
+    if (mysqli_connect_error()){
+        die("desila se gresika prilikom konektovanja na bazu podataka");
+    }
+
+
+
+//    mysqli_query($baza,"INSERT INTO korisnici (email,lozinka,datumRodjenja) VALUES ('proba@gmail.com','123456','1994-04-22')");
+
+
+    $ime = "krompir";
+    $opis = "domaci krompir prve klase";
+    $cena = 100.22;
+    $datum_nabavke = "2000-03-02";
+    $kolicina = 4;
+
+
+
+    $baza->query("INSERT INTO proizvodi (ime,opis,cena,dan_nabavke,kolicina) VALUES('$ime','$opis','$cena','$datum_nabavke','$kolicina')");
+
+
+?>
